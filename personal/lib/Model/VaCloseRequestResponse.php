33a27cec93ae196779b186f29353a429b64b9f1b
kamicup <yoshikazu.hashimoto@gmail.com>
2026-01-30 20:32:08 +0900

    警告の抑制 Implicitly marking parameter $data as nullable is deprecated, the explicit nullable type must be used instead

--- personal/lib/Model/VaCloseRequestResponse.php
+++ personal/lib/Model/VaCloseRequestResponse.php
@@ -176,7 +176,7 @@ class VaCloseRequestResponse implements ModelInterface, ArrayAccess
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->container['accept_date'] = isset($data['accept_date']) ? $data['accept_date'] : null;
     }
